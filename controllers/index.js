b2c8089638cb8c358221f49c90a73f89872cfa4c
Alex Christenson <christenson10@gmail.com>
2021-07-13 22:40:57 -0400

    ask bcs

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
-const userRoutes = require('./api/user-routes.js');
+const userRoutes = require('./api/user-routes');
 const postRoutes = require('./api/post-routes');
 const commentRoutes = require('./api/comment-routes');
+const dashboardRoutes = require('./dashboard-routes');
+const homeRoutes = require('./home-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/home', homeRoutes);
 
 module.exports = router;
